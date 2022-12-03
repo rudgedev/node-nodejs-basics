2e119419c093a41b2f0b388d0f4d4abd22d1d770
rudgedev <nekcit98@gmail.com>
2022-12-03 14:00:54 +0300

    feat: complete zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { createWriteStream } from 'fs';
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+
+const pipe = promisify(pipeline);
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const sourcePath = __dirname.concat('/files/fileToCompress.txt');
+  const destinationPath = __dirname.concat('/files/archive.gz');
+
+  const gzip = createGzip();
+  const source = createReadStream(sourcePath);
+  const destination = createWriteStream(destinationPath);
+
+  await pipe(source, gzip, destination);
 };
 
-await compress();
+await compress().catch(error => {
+  console.error('An error occured: ', error);
+  process.exitCode = 1;
+});
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+import { createWriteStream } from 'fs';
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+
+const pipe = promisify(pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const sourcePath = __dirname.concat('/files/archive.gz');
+  const destinationPath = __dirname.concat('/files/fileToCompress2.txt');
+
+  const unzip = createUnzip();
+  const source = createReadStream(sourcePath);
+  const destination = createWriteStream(destinationPath);
+
+  await pipe(source, unzip, destination);
 };
 
-await decompress();
+await decompress().catch(error => {
+  console.error('An error occured: ', error);
+  process.exitCode = 1;
+});
