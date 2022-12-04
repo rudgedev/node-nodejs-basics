96fdd308f65f1e8eb30c8143af27cda88cc14ca8
rudgedev <nekcit98@gmail.com>
2022-12-04 20:46:49 +0300

    feat: complete cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from 'child_process';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const spawnChildProcess = async args => {
+  const scriptPath = __dirname.concat('/files/script.js');
+
+  const child = fork(scriptPath);
+  child.on('message', () => {
+    process.stdout.write(args);
+  });
+
+  child.send(process.stdin);
 };
 
 spawnChildProcess();
