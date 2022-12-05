404d6d7dc1a92c8c7637b17005dbd8427d385619
rudgedev <nekcit98@gmail.com>
2022-12-05 18:24:39 +0300

    feat: complete wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import os from 'os';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const workerFilePath = __dirname.concat('/worker.js');
+
+  const cpus = os.cpus();
+
+  const number = 10;
+
+  console.log(cpus.length);
+  cpus.forEach((cpu, index) => {
+    const worker = new Worker(workerFilePath, {
+      workerData: {
+        worker_number: number + index,
+      },
+    });
+
+    worker.on('message', message => {
+      console.log({ status: 'resolved', data: message });
+    });
+
+    worker.on('error', () => {
+      console.log({ status: 'error', data: null });
+    });
+  });
 };
 
 await performCalculations();
